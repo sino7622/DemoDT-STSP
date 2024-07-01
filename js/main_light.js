2498f49c855a709fd93077f428465e5d12e5af07
07964 <chiihsuanlin@mail.sinotech.com.tw>
2024-07-01 17:43:14 +0800

    20240701
    更版

--- js/main_light.js
+++ js/main_light.js
@@ -69,22 +69,42 @@
     var myChart1 = new Chart(ctx1, {
         type: "bar",
         data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+            labels: ["2017", "2018", "2019", "2020", "2021", "2022", "2023"],
             datasets: [{
-                    label: "USA",
-                    data: [15, 30, 55, 65, 60, 80, 95],
+                    label: "積體電路",
+                    data: [5372.38, 4859.75, 4528.01, 5465.87, 7556.52, 11939.83, 12960.34],
                     backgroundColor: "rgba(16,173,139, .7)"
                 },
                 {
-                    label: "UK",
-                    data: [8, 35, 40, 60, 70, 55, 75],
+                    label: "光電",
+                    data: [2815.57, 2446.96, 2120.76, 2122.76, 2493.28, 1888.16	, 1742.04],
                     backgroundColor: "rgba(16,173,139, .5)"
                 },
                 {
-                    label: "AU",
-                    data: [12, 25, 45, 55, 65, 70, 60],
+                    label: "電腦及周邊",
+                    data: [19.87, 18.61, 160.91, 221.50, 88.34, 115.86, 146.84],
                     backgroundColor: "rgba(16,173,139, .3)"
-                }
+                },
+                {
+                    label: "通訊",
+                    data: [51.50, 54.42, 91.35, 127.29, 166.40, 211.21, 327.89],
+                    backgroundColor: "rgba(16,173,139, .3)"
+                },
+                {
+                    label: "精密機械",
+                    data: [406.34, 460.03, 412.24, 385.91, 481.10, 519.13, 510.81],
+                    backgroundColor: "rgba(16,173,139, .3)"
+                },
+                {
+                    label: "生物技術",
+                    data: [95.69, 91.52, 91.87, 125.90, 134.01, 127.31, 120.61],
+                    backgroundColor: "rgba(16,173,139, .3)"
+                },
+                {
+                    label: "其他",
+                    data: [26.23, 25.14, 27.22, 28.09, 29.19, 32.25, 46.47],
+                    backgroundColor: "rgba(16,173,139, .3)"
+                },
             ]
             },
         options: {
@@ -93,24 +113,25 @@
     });
 
 
-    // Salse & Revenue Chart
-    var ctx2 = $("#salse-revenue").get(0).getContext("2d");
+    // sun Chart
+    var ctx2 = $("#sun").get(0).getContext("2d");
     var myChart2 = new Chart(ctx2, {
         type: "line",
         data: {
-            labels: ["2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+            labels: ["2018","2019","2020", "2021", "2022", "2030(目標)"],
             datasets: [{
-                    label: "Salse",
-                    data: [15, 30, 55, 45, 70, 65, 85],
+                    label: "MW",
+                    data: [ 27.853,43.919,54.519,59.140,63.675, 84],
                     backgroundColor: "rgba(16,173,139, .7)",
                     fill: true
-                },
-                {
-                    label: "Revenue",
-                    data: [99, 135, 170, 130, 190, 180, 270],
-                    backgroundColor: "rgba(16,173,139, .5)",
-                    fill: true
-                }
+                }    
+                // ,
+                // {
+                //     label: "Revenue",
+                //     data: [99, 135, 170, 130, 190, 180, 270],
+                //     backgroundColor: "rgba(16,173,139, .5)",
+                //     fill: true
+                // }
             ]
             },
         options: {
